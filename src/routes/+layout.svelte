9bb6eeb2a75f83605a164fab648d05b5a38abd42
cataclysm-interactive <untoldtitan3k@gmail.com>
2022-10-16 13:05:42 -0600

    Let's get to work

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,76 @@
+<div class="primaryB topbar">
+    <div class="title">
+        <h1>Titan</h1>
+        <p>Software Developer</p>
+    </div>
+    <div class="nav">
+        <button>Home</button>
+        <button>Who?</button>
+        <button>The Lab</button>
+    </div>
+</div>
+
+<slot />
+
+<style>
+    /* The Global Theme */
+    :global(.primaryB) {
+        background-color: #1b3059;
+    }
+    :global(.secondaryB) {
+        background-color: #395273;
+    }
+    :global(.primaryG) {
+        background-color: #143840;
+    }
+    :global(.secondaryG) {
+        background-color: #2d5949;
+    }
+    :global(.accent) {
+        background-color: #f2e3c4;
+    }
+    /* :global(div) {
+        color: #ffffff;
+    } */
+    /* From Spara's Headquarters Alt-Art. */
+
+    /* Turns out theres a "User Agent Stylesheet" that ships with the browser. I need to override it */
+    :global(body) {
+        margin: 0;
+        font-family: "Montserrat", sans-serif;
+        color: #ffffff;
+    }
+    :global(h1) {
+        margin: 0;
+    }
+    :global(p) {
+        margin: 0;
+    }
+    :global(button) {
+        padding: 7px 15px;
+        border: 0;
+        border-radius: 2px;
+        margin-right: 40px;
+        background-color: #3d717c;
+        color: #ffffff;
+    }
+
+    /* Actual styles related to the topbar */
+    .topbar {
+        width: 100%;
+        height: fit-content;
+        display: flex;
+    }
+
+    .title {
+        padding-left: 15px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    .nav {
+        margin-left: auto;
+        margin-right: 1%;
+        margin-top: 20px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Home Page -->
+<style>
+  h1{
+    margin-top: 35px;
+    text-align: center;
+    font-size: 50px;
+  }
+  h2{
+    text-align: center;
+    font-size: 20px;
+  }
+  h3{
+    text-align: center;
+    margin-top: 25px;
+    font-size: 25px;
+  }
+</style>
+
+<div>
+  <h1>Do you like technology?</h1>
+  
+  <h2>If yes, then you'll like this site.</h2>
+  <h3>I'm Titan, and this is my humble site, where I write about things that interest me, in the wonderful world of tech</h3>
+</div>
