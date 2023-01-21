4d11e648d1adb3c2ffc4455f582f29340920f1d2
Mitchell Miller <95239376+untold-titan@users.noreply.github.com>
2023-01-21 18:16:38 +0000

    NEW SITE DESIGN

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script>
+
+</script>
+<div>
+  <div class="title">
+    <h1>Titan - Software Developer</h1>
+  </div>
+  <button class="arrow-button"><img class="arrow-img" src="/arrow.png" alt="arrow pointing to the right"></button>
+</div>
+
+<style>
+  .title{
+    position: absolute;
+    top:35%;
+    left:0;
+    right:0
+  }
+  .arrow-img{
+    width: 50px;
+  }
+  
+  @keyframes button-hover{
+    from{
+      width:100px;
+      height: 70px;
+    }
+    to{
+      width:120px;
+      height:90px;
+    }
+  }
+  @keyframes button-hover{
+    from{
+      width:100px;
+      height: 70px;
+    }
+    to{
+      width:120px;
+      height:90px;
+    }
+  }
+  
+  .arrow-button{
+    width:100px;
+    height:70px;
+  }
+
+  .arrow-button:hover{
+    width:120px;
+    height:90px;
+    animation-name: button-hover;
+    animation-duration: .5s;
+  }
+</style>
