fe2420ee880e94f4dd303a42f0333713d33e501d
Titan <untoldtitan3k@gmail.com>
2023-01-21 11:48:22 -0700

    Damn, codespaces works better than my own laptop :(

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,79 @@
 <script>
+  import { slide, fade } from "svelte/transition";
 
+  let homepage = false;
 </script>
-<div>
+
+{#if homepage != true}
   <div class="title">
-    <h1>Titan - Software Developer</h1>
+    <div>
+      <h1>Titan - Software Developer</h1>
+    </div>
   </div>
-  <button class="arrow-button"><img class="arrow-img" src="/arrow.png" alt="arrow pointing to the right"></button>
-</div>
+  <div class="title">
+    <button
+      out:fade
+      class="arrow-button"
+      on:click={() => {
+        homepage = true;
+      }}
+      ><img
+        class="arrow-img"
+        src="/arrow.png"
+        alt="arrow pointing to the right"
+      /></button
+    >
+  </div>
+{/if}
+{#if homepage}
+  <div in:fade={{ delay: 400 }}>
+    <h1>Homepage</h1>
+  </div>
+{/if}
 
 <style>
-  .title{
-    position: absolute;
-    top:35%;
-    left:0;
-    right:0
+  .title {
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
   }
-  .arrow-img{
+  .arrow-img {
+    position: static;
     width: 50px;
   }
-  
-  @keyframes button-hover{
-    from{
-      width:100px;
+
+  @keyframes button-hover {
+    from {
+      width: 100px;
       height: 70px;
     }
-    to{
-      width:120px;
-      height:90px;
+    to {
+      width: 120px;
+      height: 90px;
     }
   }
-  @keyframes button-hover{
-    from{
-      width:100px;
+  @keyframes button-unhover {
+    to {
+      width: 100px;
       height: 70px;
     }
-    to{
-      width:120px;
-      height:90px;
+    from {
+      width: 120px;
+      height: 90px;
     }
-  }
-  
-  .arrow-button{
-    width:100px;
-    height:70px;
   }
 
-  .arrow-button:hover{
-    width:120px;
-    height:90px;
+  .arrow-button {
+    width: 100px;
+    height: 70px;
+    animation-name: button-unhover;
+    animation-duration: 0.75s;
+  }
+
+  .arrow-button:hover {
+    width: 120px;
+    height: 90px;
     animation-name: button-hover;
-    animation-duration: .5s;
+    animation-duration: 0.5s;
   }
 </style>
